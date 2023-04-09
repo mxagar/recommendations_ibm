c618c727f0eecb9ad4f3d8a94e57b5efc5090f0a
Mikel <mxagar@gmail.com>
2023-04-09 15:27:59 +0200

    feat: package files, tbd

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="article_recommender",
+    version="0.1.0",
+    description="Article Recommender System for the IBM Watson Platform.",
+    author="Mikel Sagardia",
+    packages=['article_recommender'], # folder names
+	zip_safe=False
+)
